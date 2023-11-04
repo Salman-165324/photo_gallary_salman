8b27a05f502f25e08817f00fd096c39141326865
salman165324 <salmansadi165324@gmail.com>
2023-11-05 04:04:39 +0600

    Adde daisyUI

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -1,36 +1,41 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import { data } from "@/data/image";
 import SortableImages from "./SortableImages";
 import { DndContext, closestCenter } from "@dnd-kit/core";
-import { SortableContext, arrayMove, rectSortingStrategy } from "@dnd-kit/sortable";
+import {
+  SortableContext,
+  arrayMove,
+  rectSortingStrategy,
+} from "@dnd-kit/sortable";
 
 const Gallery = () => {
   const [images, setImage] = useState(data);
 
-
-
-// Event handler for onDragEnd event listener. It is fired after a draggable item is dropped. 
-// onDragEnd event does not move draggable items into droppable containers. 
-// Rather, it provides information about which draggable item was dropped and whether it was over a droppable container when it was dropped.
+  // Event handler for onDragEnd event listener. It is fired after a draggable item is dropped.
+  // onDragEnd event does not move draggable items into droppable containers.
+  // Rather, it provides information about which draggable item was dropped and whether it was over a droppable container when it was dropped.
 
   const onDragEnd = (event) => {
 
+    //  over: Draggable item on which a dragged item was dropped over. If it wasn't dropped on anything it will be null.
+    // active: the dragged item.
+    
+    const { active, over } = event;
+    if (active.id === over.id) {
+      return;
+    }
 
-    console.log(event); 
-    //  over: Draggable Area or Id on which draggable item was dropped over. If it wasn't dropped on anything it will be null. 
-     const {active, over} = event; 
-     if ( active.id === over.id){
-        return; 
-     }
-      
-     setImage((images) => {
-          const oldIndexOfDraggedItem = images.findIndex(image => image.id === active.id)
-          const newIndexOfDraggedItem = images.findIndex(image => image.id === over.id) 
-          return arrayMove(images, oldIndexOfDraggedItem, newIndexOfDraggedItem); 
-     })
-
-  }
+    setImage((images) => {
+      const oldIndexOfDraggedItem = images.findIndex(
+        (image) => image.id === active.id
+      );
+      const newIndexOfDraggedItem = images.findIndex(
+        (image) => image.id === over.id
+      );
+      return arrayMove(images, oldIndexOfDraggedItem, newIndexOfDraggedItem);
+    });
+  };
 
   return (
     <section className="my-20 flex flex-col items-center justify-center">
@@ -49,10 +54,17 @@ const Gallery = () => {
         {/* Gallery Picture container */}
 
         <div className="gallery-box-padding grid grid-cols-3 gap-y-4">
-          <DndContext collisionDetection={closestCenter} onDragEnd={onDragEnd} >
+
+          {/* The <DndContext> provider makes use of the React Context API to share data between draggable and droppable components and hooks. */}
+          <DndContext collisionDetection={closestCenter} onDragEnd={onDragEnd}>
+
+            {/* The SortableContext provides information via context that is consumed by the useSortable hook. */}
             <SortableContext items={images} strategy={rectSortingStrategy}>
               {images.map((image) => (
+
+                // SortableImages is a custom component which internally uses useSortable hook from dnd kit to Manage drag, drop and sorting
                 <SortableImages key={image.id} image={image}></SortableImages>
+                
               ))}
             </SortableContext>
           </DndContext>
--- components/SortableImages.jsx
+++ components/SortableImages.jsx
@@ -2,10 +2,20 @@ import { useSortable } from "@dnd-kit/sortable";
 import Image from "next/image";
 import React from "react";
 import {CSS} from "@dnd-kit/utilities"
+
+
 const SortableImages = ({ image }) => {
+
+  // Argument {id:image.id} the id argument works as a identifier. 
+  // The argument passed to the id argument of useSortable should match the id passed in the items array of the parent SortableContext provider.
+  
+  // attributes, listeners and other properties which is destructured from useSortable hook has good documentation on dnd's doc
+
+
   const {
     attributes, 
     listeners, 
+    
     setNodeRef, 
     transform, 
     transition, 
