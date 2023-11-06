44ddd734c2a215c21437da13f1a95e4674a7d12d
salman165324 <salmansadi165324@gmail.com>
2023-11-06 19:03:26 +0600

    Added a tailwind equivalent of touch-action none

--- components/SortableImages.jsx
+++ components/SortableImages.jsx
@@ -28,17 +28,15 @@ const SortableImages = ({
   const style = {
     transition,
     transform: CSS.Transform.toString(transform),
-    touchAction: "none"
   };
 
   const handleCheckboxChange = () => {
     // toggling the isChecked state. As the initial value is set to false it will correctly hold the actual state of the checkbox element even when it will be changed multiple of time.
 
-
     if (checkboxRef.current.checked) {
       const isAlreadyAdded = selectedImagesForDelete.includes(image.id);
       if (!isAlreadyAdded) {
-        // used Array destructure method so that we don't use the same reference. Now a copy of the items of selectedImagesForDelete array will be passed. 
+        // used Array destructure method so that we don't use the same reference. Now a copy of the items of selectedImagesForDelete array will be passed.
         const newArray = [...selectedImagesForDelete];
 
         newArray.push(image.id);
@@ -66,7 +64,7 @@ const SortableImages = ({
         {...listeners}
         {...attributes}
         style={style}
-        className=" relative h-[200px] w-[200px] overflow-hidden rounded-lg border-2 border-black "
+        className=" pointer-events-none relative h-[200px] w-[200px] overflow-hidden rounded-lg border-2 border-black "
       >
         <Image key={image.id} src={image.url} alt="" fill></Image>
       </div>
